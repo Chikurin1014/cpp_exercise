b74e142ef8ebe0f03873cbb1cf862dd651209230
Chikurin1014 <109526285+Chikurin1014@users.noreply.github.com>
2025-06-08 11:56:54 +0000

    :sparkles: 演習問題とテストを実装

--- test/02/0202_test.cpp
+++ test/02/0202_test.cpp
@@ -1,8 +1,76 @@
 #include "02-02_primitive-types.hpp"
 
+#include <sstream>
 #include <gtest/gtest.h>
 
-TEST(ch_0202, primitive_types) {
-    // TODO: Implement the test
-    ASSERT_TRUE(true);
+TEST(ch_0202, boolean) {
+    testing::internal::CaptureStdout();
+
+    boolean(); // from 02-02_primitive-types.hpp
+
+    auto output = testing::internal::GetCapturedStdout();
+    
+    auto ss = std::stringstream(output);
+    auto output1 = std::string();
+    auto output2 = std::string();
+    std::getline(ss, output1);
+    std::getline(ss, output2);
+
+    ss.str("");
+    ss.clear();
+    ss << "Type of is_true: " << typeid(bool).name() << "   Value of is_true: 1";
+    auto answer1 = ss.str();
+
+    ss.str("");
+    ss.clear();
+    ss << "Type of is_false: " << typeid(bool).name() << "   Value of is_false: 0";
+    auto answer2 = ss.str();
+
+    EXPECT_EQ(output1, answer1);
+    EXPECT_EQ(output2, answer2);
+}
+
+TEST(ch_0202, character) {
+    testing::internal::CaptureStdout();
+
+    character(); // from 02-02_primitive-types.hpp
+
+    auto output = testing::internal::GetCapturedStdout();
+    output.erase(output.find_last_not_of("\n") + 1); // Trim trailing whitespace
+
+    auto ss = std::stringstream();
+    ss << "Type of character: " << typeid(char).name() << "   Value of character: a";
+    auto answer = ss.str();
+
+    EXPECT_EQ(output, answer);
+}
+
+TEST(ch_0202, integer) {
+    testing::internal::CaptureStdout();
+
+    integer(); // from 02-02_primitive-types.hpp
+
+    auto output = testing::internal::GetCapturedStdout();
+    output.erase(output.find_last_not_of("\n") + 1); // Trim trailing whitespace
+
+    auto ss = std::stringstream();
+    ss << "Type of integer: " << typeid(int).name() << "   Value of integer: 42";
+    auto answer = ss.str();
+
+    EXPECT_EQ(output, answer);
+}
+
+TEST(ch_0202, floating_point) {
+    testing::internal::CaptureStdout();
+
+    floating_point(); // from 02-02_primitive-types.hpp
+
+    auto output = testing::internal::GetCapturedStdout();
+    output.erase(output.find_last_not_of("\n") + 1); // Trim trailing whitespace
+
+    auto ss = std::stringstream();
+    ss << "Type of floating_point: " << typeid(float).name() << "   Value of floating_point: 3.14";
+    auto answer = ss.str();
+
+    EXPECT_EQ(output, answer);
 }
