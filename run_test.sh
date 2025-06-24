f71570f52b5602c59ec2d2b6dcb0c96ffcc4e9b6
Chikurin1014 <chikurin1014@gmail.com>
2025-06-24 15:43:22 +0900

    :adhesive_bandage: ninja-buildをインストールするよう指示を追加

--- run_test.sh
+++ run_test.sh
@@ -3,21 +3,28 @@
 src="${BASH_SOURCE:-$0}"
 cd "$(dirname "${src}")"
 
-if [ ! $1 ]; then
-    echo "使い方: ${src} <章番号>"
-    echo "例:     ${src} 0102  ... 01-02_hello_worldの演習問題のテストを実行"
+ch="$1"
+dir="build/test/${1:0:2}"
+
+which ninja > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "ビルドツール\`ninja\`が見つかりません。以下のコマンドからインストールしてください:" 1>&2
+    echo "sudo apt install ninja-build" 1>&2
     exit 1
 fi
 
-ch="$1"
-dir="build/test/${1:0:2}"
+if [ ! $1 ]; then
+    echo "テストを実行する章番号を指定してください。"
+    echo "使い方: ${src} <章番号>" 1>&2
+    echo "例:     ${src} 0102  ... 01-02_hello_worldの演習問題のテストを実行" 1>&2
+    exit 1
+fi
 
 if [ ! -d "build" ]; then
     cmake -S . -B build -G Ninja
 fi
 
-cmake --build build --parallel $(nproc)
-
+cmake --build build --parallel $(nproc) 
 exe="${dir}/${ch}_test"
 if [ ! -f "$exe" ]; then
     echo "章番号 ${ch} のテストファイル(${exe})が見つかりません"
